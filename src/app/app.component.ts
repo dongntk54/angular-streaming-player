dcb875405aedf39fed294446b6211c1bc16b3fb3
Dongnt54 <dongntk54@gmail.com>
2020-08-29 01:07:54 +0700

    add width and height options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   options = {
     autoplay: true,
     controls: false,
+    width: 640,
+    height: 360,
     sources: [
       {
         src: "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
